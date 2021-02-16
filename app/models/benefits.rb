c91e51f33c3c3eb645910062278cf43678e4de54
mpg-kentaro-kanazashi <kentaro.kanazashi@medpeer.co.jp>
2021-02-16 15:13:03 +0900

    1-b修正

--- app/models/benefits.rb
+++ app/models/benefits.rb
@@ -11,11 +11,9 @@ class Benefits < ApplicationRecord
   end
 
   def self.make_backup(file, data_path, full_file_name)
-    if File.exist?(full_file_name)
-      silence_streams(STDERR) { system("cp #{full_file_name} #{data_path}/bak#{Time.zone.now.to_i}_#{file.original_filename}") }
-    end
+    FileUtils.cp "#{full_file_name}", "#{data_path}/bak#{Time.zone.now.to_i}_#{file.original_filename}"
   end
-
+  
   def self.silence_streams(*streams)
     on_hold = streams.collect { |stream| stream.dup }
     streams.each do |stream|
